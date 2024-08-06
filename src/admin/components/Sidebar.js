23f34c7a0ff19a88fc2fc06f97f0d29856373568
HoaiTam30102003 <hoaitam30102003@gmail.com>
2024-08-06 13:52:28 +0700

    fix url

--- src/admin/components/Sidebar.js
+++ src/admin/components/Sidebar.js
@@ -35,8 +35,8 @@ const Sidebar = () => {
           <Nav.Link as={NavLink} to="/admin/user" className="sidebar-link">
             <FontAwesomeIcon icon={faList} className="me-2" /> Người dùng
           </Nav.Link>
-          <Nav.Link as={NavLink} to="/admin/danhsach" className="sidebar-link">
-            <FontAwesomeIcon icon={faList} className="me-2" /> Danh Sách
+          <Nav.Link as={NavLink} to="/admin/product" className="sidebar-link">
+            <FontAwesomeIcon icon={faList} className="me-2" /> Danh sách
           </Nav.Link>
           <Nav.Link as={NavLink} to="/admin/billing" className="sidebar-link" activeClassName="active-link">
             <FontAwesomeIcon icon={faFileInvoiceDollar} className="me-2" /> Billing
