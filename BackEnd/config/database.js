6a2eb070470c16bc6222ff5cf53bc03c9708e5b5
baongpc05497 <145195574+giabao204@users.noreply.github.com>
2024-08-06 23:08:37 +0700

    Thêm Giỏ Hàng và From Thanh Toán

--- BackEnd/config/database.js
+++ BackEnd/config/database.js
@@ -2,7 +2,7 @@
 const { Sequelize } = require('sequelize');
 
 // Cấu hình kết nối đến cơ sở dữ liệu PostgreSQL
-const sequelize = new Sequelize('reactjs', 'root', 'root', {
+const sequelize = new Sequelize('reactjs', 'root', 'mysql', {
     host: 'localhost',
     dialect: 'mysql'
 });
